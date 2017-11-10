5073ec02a732cd9d3eced94ee38636a15ea3e0b1
tongyy <tony0x00@gmail.com>
2017-11-10 15:12:34 +0800

    remove unused import

--- src/test/java/com/tongyy/QueueAndTopicTest.java
+++ src/test/java/com/tongyy/QueueAndTopicTest.java
@@ -3,7 +3,6 @@ package com.tongyy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessagePostProcessor;
